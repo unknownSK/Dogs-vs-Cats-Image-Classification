97bf78e08edb16a9d28369a5e3faaf75e9d242d6
Suryakant Tibrewal <37638352+unknownSK@users.noreply.github.com>
2018-12-21 13:51:52 +0530

    Update cat vs dog.py

--- cat vs dog.py
+++ cat vs dog.py
@@ -1,3 +1,5 @@
+""" Author = Suryakant Tibrewal (unknownSK)"""
+
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
