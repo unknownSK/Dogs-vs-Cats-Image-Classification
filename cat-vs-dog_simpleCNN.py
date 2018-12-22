fc87d0e823436e28575d38431c76a696baf5e111
Suryakant Tibrewal <37638352+unknownSK@users.noreply.github.com>
2018-12-22 13:43:23 +0530

    Simple CNN

--- cat-vs-dog_simpleCNN.py
+++ cat-vs-dog_simpleCNN.py
@@ -35,7 +35,7 @@ model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
-model.add(Dense(64))
+model.add(Dense(64)) 
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(1))
